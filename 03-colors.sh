d79d65fa5ed7c809a87aa9daec19116639279559
sumana <mhsumana@gmail.com>
2023-08-03 06:19:34 +1000

    Auto Push

--- 03-colors.sh
+++ 03-colors.sh
@@ -18,9 +18,8 @@
 # Ex : echo -e "\e[backgroundcode;foregroundcodem your msg\e[0m"
   echo -e "\e[43;35m printing yellow back and magenta foreground\e[0m"
 
-  echo -e"\e[33m I am printing Yellow color\e[0m"
+  echo -e "\e[33m I am printing Yellow color\e[0m"
   echo -e "\e[32m I am printing green color\e[0m"
   echo -e "\e[34m I am printing blue color\e[0m"
 
   echo -e "\e[43,34m I am printing both color\e[0m"
-  
