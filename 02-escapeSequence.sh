d883f52780c5db3385ed358ed694e3fb137e96cf
sumana <mhsumana@gmail.com>
2023-08-02 12:00:06 +1000

    Auto Push

---
+++ 02-escapeSequence.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Welcome to Bash Scripting
+
+# This is how we can execute the scripts
+# $ bash scriptName.sh  or  sh scriptName.sh  or  ./scriptName.sh
+
+# \n : This indicates a new line
+
+echo Hi, \n How are you ?
+
+echo Line1
+echo Line2
