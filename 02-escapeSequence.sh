3ea87c34e8f71e2e52355e2f98591fc8d6165101
sumana <mhsumana@gmail.com>
2023-08-02 12:18:51 +1000

    Auto Push

--- 02-escapeSequence.sh
+++ 02-escapeSequence.sh
@@ -6,8 +6,16 @@ echo Welcome to Bash Scripting
 # $ bash scriptName.sh  or  sh scriptName.sh  or  ./scriptName.sh
 
 # \n : This indicates a new line
+# \t  : This gives a tab space 
 
 echo -e "Hi,\nHow are you ?"
 
 echo Line1
 echo Line2
+
+echo -e "Line5\nLine6"
+# Whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " "  : Double Quotes 
+# ' '  : Single Quotes
+
+echo -e "Serial no\n1\2\tProduct\Pen\n\Pencil"
