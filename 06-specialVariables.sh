673dd66b204edcf0680e9a469c5dafede9170d1e
sumana <mhsumana@gmail.com>
2023-08-03 11:18:57 +1000

    Auto Push

--- 06-specialVariables.sh
+++ 06-specialVariables.sh
@@ -7,3 +7,9 @@
 echo $0                            # $0 prints the script name you're executing
 echo "Executed Script Name is: $0"
 
+# ROCKET_NAME=Chandrayaan
+# bash scriptName.sh  arg1  arg2  arg3
+
+echo "Name of recently launched rocket is $1"
+echo "Popular EV vehicle in States is $2"
+echo "current Topic is $3"
