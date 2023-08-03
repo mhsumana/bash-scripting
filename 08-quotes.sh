81aee04a33c98a4a60944baaca61315836384e5d
sumana <mhsumana@gmail.com>
2023-08-03 11:47:31 +1000

    Auto Push

--- 08-quotes.sh
+++ 08-quotes.sh
@@ -6,5 +6,11 @@ echo '$?'    # $? is going to print the exit code of the last command.
 echo '$*'    # $* is going to print the used arguments
 echo '$@'   # $* is going to print the used arguments
 
+echo "$$"    # $$ is going to print the PID of the current proces 
+echo "$#"    # $# is going to print the number of arguments
+echo "$?"    # $? is going to print the exit code of the last command.
+echo "$*"    # $* is going to print the used arguments
+echo "$@"   # $* is going to print the used arguments
+
 # Single Quotes will suppress the power of a special variable
 # Whenever you have any special variables, prefer to avoid single quotes and go with double quotes
