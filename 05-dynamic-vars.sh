5bf53f47856092864627949264caa685b2683da0
sumana <mhsumana@gmail.com>
2023-08-03 11:00:35 +1000

    Auto Push

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -8,3 +8,6 @@ echo "Total number of open sessions is $SESSIONS_COUNT "
 
 # To print the date is color
 echo -e "\e[33m Todays date is $DATE \e[0m"
+echo -e "Todays date is \e[33m $DATE \e[0m"
+
+echo -e "Total number od open sessions is \e[34m $SESSION_COUNT \e[0m"
