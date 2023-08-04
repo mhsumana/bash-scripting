c11197baa13bf7b4ad6adf1de587a4becaf24c37
sumana <mhsumana@gmail.com>
2023-08-05 07:57:26 +1000

    Auto Push

---
+++ 12-conditions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Conditions helps us to execute something only if some factor is true and if that factor is false then that something wont be executed. 
+
+# Syntax of CASE
+# case $var in
+#     opt1) commands-x ;;
+#     opt2) commands-y ;;
+# esac
+
+
+
+case $ACTION in
+    start)
+       echo "Starting payment service"
+       ;;
+    stop)
+       echo "Stopping payment service"
+       ;;
+    restart 
+       echo "Restart payment service"
+       ;;
+esac
+
+
