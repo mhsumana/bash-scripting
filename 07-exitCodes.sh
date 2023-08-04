b9df17c8149844acdc2ab7df2263bb1a1b754081
sumana <mhsumana@gmail.com>
2023-08-04 15:03:44 +1000

    Auto Push

--- 07-exitCodes.sh
+++ 07-exitCodes.sh
@@ -4,3 +4,8 @@
 # 0 to 255 is the range of the codes 
 # Among all,  0  represents action completed successfull .
 # Anything in betwenn 1 to 255 represents either partial succes, parital failure , complete failure
+
+# Exit codes also plays a key role in debugging big scripts.
+
+# Ex : If you're having a big script and if your script fails and it's really challenging to figure out the mistake as it makes us to watch over the entie script from the beginning.
+# Instead, we can use exit codes everyWhere to find out where our script failes.
