6c161addd8d345ce7f31e371dc5727579ca73e0d
sumana <mhsumana@gmail.com>
2023-08-05 08:11:40 +1000

    Auto Push

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -21,8 +21,8 @@ case $ACTION in
        echo -e "\e[33m Restart payment service\e[0m"
        ;;
     *)
-       echo "Valid options are start or stop or restart"
-       echo -e "Example Usage:\n \t bash scriptName start"
+       echo -e "\e[35m Valid options are start or stop or restart\e[0m"
+       echo -e "\e[33m Example Usage:\n \t bash scriptName start\e[0m"
        ;;
 esac
 
