ad75d3b7fd90d27ff4a497034d2d418f4261b92d
sumana <mhsumana@gmail.com>
2023-08-23 13:32:37 +1000

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,15 +8,17 @@ if [ $USER_ID -ne 0 ]; then
     echo -e "\e[31m Script is expected to be executed by the root user or with a sudo privilege\e[0m \n \t Example: sudo bash wrapper.sh frontend"
     exit 1 
 fi
-echo -e "\e[35m Configuring frontend\e[0m \n"
 
 stat() { 
     if [ $1 -eq 0 ]; then
       echo -e "\e[32m success\e[0m"
     else
       echo -e "\e[31m Failure\e[0m" 
+      exit 2
     fi     
 }
+
+echo -e "\e[35m Configuring frontend\e[0m \n"
 
 echo -n "Installing Frontend:"
 yum install nginx -y &>> /tmp/frontend.log
