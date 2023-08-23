ad14c5d6279b790516ac7c2260a28476d9d3247e
sumana <mhsumana@gmail.com>
2023-08-23 12:16:07 +1000

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Validate the user who is running the script is a root user or not.
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo -e "\e[31m Script is expected to be executed by the root user or with a sudo privilege\e[0m \n \t Example: sudo wrapper.sh frontend"
+    exit 1 
+fi
 echo "Configuring frontend"
 yum install nginx -y
 
